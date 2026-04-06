9f001ca3213b224596813277cb769ef38dc13bbd
Jill Wang <jaw185@duke.edu>
2026-04-05 23:35:39 -0400

    maximum cursor size

--- final_project_vga_files/FinalProjectVGAProcessor.v
+++ final_project_vga_files/FinalProjectVGAProcessor.v
@@ -225,10 +225,10 @@ module FinalProjectVGAProcessor(
     assign sx = $signed({2'b00, x});
     assign sy = $signed({3'b000, y});
     assign cursorDrawSize =
-        (penSize == 3'd1) ? 7'd40 :
-        (penSize == 3'd2) ? 7'd66 :
-        (penSize == 3'd3) ? 7'd92 :
-        (penSize == 3'd4) ? 7'd126 : 7'd160;
+        (penSize == 3'd1) ? 7'd40 : //0.2
+        (penSize == 3'd2) ? 7'd66 : //0.4
+        (penSize == 3'd3) ? 7'd92 : //0.6
+        (penSize == 3'd4) ? 7'd116 : 7'd128;
     assign cursorLeft = $signed({2'b00, cursorX, 3'b000}) + 12'sd4 - ($signed({5'b0, cursorDrawSize}) >>> 1);
     assign cursorTop = $signed({3'b000, cursorY, 3'b000}) + 12'sd4 - ($signed({5'b0, cursorDrawSize}) >>> 1);
 
